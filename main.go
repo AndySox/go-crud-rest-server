9611a05c865f408ceb7ce226a79e2295fa0c7617
AdminGoCom <admin@gocommercial.co.za>
2019-03-29 21:23:13 +0000

    adding the delete function, also some updates to the update function.

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Basic User struct that passes json
 type User struct {
 	Id       string `json:"id,omitempty"`
 	Username string `json:"username,omitempty"`
 	Email    string `json:"email,omitempty"`
 }
 
+// slice of user structs
 var users []User
 
 func createUser(w http.ResponseWriter, r *http.Request) {
@@ -40,21 +42,39 @@ func listUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	//ignore errors for this version
+	for i, user := range users {
+		if user.Id == params["id"] {
+			//ignore errors for this version
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			users[i] = user
+			json.NewEncoder(w).Encode(users)
+			return
+		}
+	}
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	//ignore errors for this version
+	for i, user := range users {
+		if user.Id == params["id"] {
+			users = append(users[:i], users[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
 }
 
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/create", createUser).Methods("POST")
-	router.HandleFunc("/get/{:id}", getUser).Methods("GET")
+	router.HandleFunc("/get/{id}", getUser).Methods("GET")
 	router.HandleFunc("/list", listUsers).Methods("GET")
-	router.HandleFunc("/update/{:id}", updateUser).Methods("PUT")
-	router.HandleFunc("/delete/{:id}", createUser).Methods("DELETE")
+	router.HandleFunc("/update/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/delete/{id}", deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
